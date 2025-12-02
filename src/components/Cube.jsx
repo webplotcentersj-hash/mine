9b1602be2c2df6374b5344215de87160f2d9584e
webplotcentersj-hash <web.plotcentersj@gmail.com>
2025-12-02 13:38:35 -0300

    Convertir textura square en sol que emite luz

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -2,6 +2,7 @@ import { useStore } from '../hooks/useStore.js'
 import { useBox } from '@react-three/cannon'
 import { useState } from 'react'
 import * as textures from '../images/textures.js'
+import { PointLight } from 'three'
 
 export const Cube = ({ id, position, texture }) => {
   const [isHovered, setIsHovered] = useState(false)
@@ -13,33 +14,47 @@ export const Cube = ({ id, position, texture }) => {
   }))
 
   const activeTexture = textures[texture + 'Texture']
+  const isSun = texture === 'square'
 
   return (
-    <mesh
-      onPointerMove={(e) => {
-        e.stopPropagation()
-        setIsHovered(true)
-      }}
-      onPointerOut={(e) => {
-        e.stopPropagation()
-        setIsHovered(false)
-      }}
-      ref={ref}
-      onClick={(e) => {
-        e.stopPropagation()
+    <>
+      <mesh
+        onPointerMove={(e) => {
+          e.stopPropagation()
+          setIsHovered(true)
+        }}
+        onPointerOut={(e) => {
+          e.stopPropagation()
+          setIsHovered(false)
+        }}
+        ref={ref}
+        onClick={(e) => {
+          e.stopPropagation()
 
-        if (e.altKey) {
-          removeCube(id)
-        }
-      }}
-    >
-      <boxBufferGeometry attach='geometry' />
-      <meshStandardMaterial
-        color={isHovered ? 'grey' : 'white'}
-        transparent
-        map={activeTexture}
-        attach='material'
-      />
-    </mesh>
+          if (e.altKey) {
+            removeCube(id)
+          }
+        }}
+      >
+        <boxBufferGeometry attach='geometry' />
+        <meshStandardMaterial
+          color={isHovered ? 'grey' : isSun ? '#FFD700' : 'white'}
+          transparent={!isSun}
+          emissive={isSun ? '#FFD700' : '#000000'}
+          emissiveIntensity={isSun ? 2 : 0}
+          map={activeTexture}
+          attach='material'
+        />
+      </mesh>
+      {isSun && (
+        <pointLight
+          position={position}
+          intensity={2}
+          color="#FFD700"
+          distance={10}
+          decay={2}
+        />
+      )}
+    </>
   )
 }
